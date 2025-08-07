1bc87204af04d44b332e9ba20d725e5ce9a20ec5
Xanthard1 <xanthard3@gmail.com>
2025-08-07 18:23:09 +1200

    V1.2 ~ Updated CurrencyGrindEvent to CurrencyGrindAsyncEvent

--- src/main/java/net/vulcandev/vulcanapi/vulcantools/events/CurrencyGrindAsyncEvent.java
+++ src/main/java/net/vulcandev/vulcanapi/vulcantools/events/CurrencyGrindAsyncEvent.java
@@ -8,20 +8,21 @@ import org.jetbrains.annotations.NotNull;
 /**
  * Event fired when a player earns currency from tool usage.
  */
-public class CurrencyGrindEvent extends Event {
+public class CurrencyGrindAsyncEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
     private final Player player;
     private final String currency;
     private long amount;
 
     /**
-     * Creates a new CurrencyGrindEvent.
+     * Creates a new CurrencyGrindAsyncEvent.
      *
      * @param player the player who earned the currency
      * @param currency the type of currency earned
      * @param amount the amount of currency earned
      */
-    public CurrencyGrindEvent(Player player, String currency, long amount) {
+    public CurrencyGrindAsyncEvent(Player player, String currency, long amount) {
+        super(true); // Mark as async event
         this.player = player;
         this.currency = currency;
         this.amount = amount;
